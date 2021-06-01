6739cecbf7ca7f2f7e4f6adb20f981ce84901b83
mori <ystk0916amr@gmail.com>
2021-06-01 15:07:18 +0900

    st

--- config/settings.py
+++ config/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     STATIC_DIR
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
+STATIC_ROOT = '/var/www/testvps/static/admin'
 
 # STATIC_ROOT = '/var/www/{}/static'.format(english-word)
 
